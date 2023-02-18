dff2c3c82125cba506174668d544663f3226420b
Timchenko Bogdan <tbd2504@.gmail.com>
2023-02-18 12:01:17 +0200

    Add new font

---
+++ game/fonts/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+import pygame
+
+from game.config import FONT_SIZE
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+pygame.font.init()
+
+bionicle = pygame.font.Font('bionicle.ttf', FONT_SIZE)
--- game/objects/views/text_surface.py
+++ game/objects/views/text_surface.py
@@ -1,43 +1,39 @@
-from pygame.font import SysFont
+from pygame.font import Font
 
 from game.config import *
-from game.utils.decorators import is_value_type, is_values_type
+from game.fonts import bionicle
+from game.utils.decorators import is_value_type
 from game.utils.functions import get_valid_value
 from game.utils.custom_type import color_code
 from game.objects.base_object import BaseObject
 
 
 class TextSurface(BaseObject):
-    def __init__(self, text: str = '', *, font_name: str = FONT_NAME, font_size: int = FONT_SIZE,
-                 color: color_code = WHITE, bg_color: color_code = None):
-        self.text = get_valid_value(text, str)
-        self.color = get_valid_value(color, str)
-        self.bg_color = get_valid_value(bg_color, str | None)
-        self.set_font(font_name, font_size)
+    def __init__(self, text: str = '', *, font: Font = bionicle, color: color_code = WHITE, bg_color: color_code = None):
+        self._text = get_valid_value(text, str)
+        self._color = get_valid_value(color, str)
+        self._bg_color = get_valid_value(bg_color, str | None)
+        self._font: Font = get_valid_value(font, Font)
         super().__init__(self._render())
 
     @is_value_type(str)
     def set_text(self, value: str):
-        self.text = value
+        self._text = value
         self.surface = self._render()
         self.rect = self.surface.get_rect()
 
     @is_value_type(color_code)
     def set_color(self, value: color_code):
-        self.color = value
+        self._color = value
         self.surface = self._render()
         self.rect = self.surface.get_rect()
 
     @is_value_type(color_code)
     def set_bg_color(self, value: color_code):
-        self.bg_color = value
+        self._bg_color = value
         self.surface = self._render()
         self.rect = self.surface.get_rect()
 
-    @is_values_type(str, int)
-    def set_font(self, name: str, size: int):
-        self.font = SysFont(name, size)
-
     def _render(self):
-        return self.font.render(self.text, False, self.color, self.bg_color)
+        return self._font.render(self._text, False, self._color, self._bg_color)
 
