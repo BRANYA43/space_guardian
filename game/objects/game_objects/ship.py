08fdc177ca2440d8407cd387c5681e35dae4ab67
Timchenko Bogdan <tbd2504@.gmail.com>
2023-02-22 14:31:00 +0200

    Refactor Ship and test

--- game/objects/game_objects/ship.py
+++ game/objects/game_objects/ship.py
@@ -1,22 +1,24 @@
 from pygame import Surface
+from pygame.sprite import Group
 
-from game.config import *
-from ..base_object.game_object import GameObject
+from ..base_objects import GameObject
 from .weapon import Weapon
-from game.utils.decorators import is_value_type, is_values_type
-from game.utils.functions import get_valid_value
+from game.utils.decorators import is_values_types, is_value_type
+from game.utils.functions import get_value_with_valid_type
+from game.config import LEFT, RIGHT
 
 
 class Ship(GameObject):
-    def __init__(self, image: Surface, weapon: Weapon, *, move_speed: int = 5, health: int = 1):
-        super().__init__(image, move_speed=move_speed)
-        self._weapon = get_valid_value(weapon, Weapon)
-        self._health = get_valid_value(health, int)
+    def __init__(self, image: Surface, weapon: Weapon, group_projectiles: Group,
+                 *, move_speed: int = 5, health: int = 1):
+        super().__init__(image, move_speed=move_speed, health=health)
+        self._weapon = get_value_with_valid_type(weapon, Weapon)
+        self._group_projectiles = get_value_with_valid_type(group_projectiles, Group)
         self._moving_left = False
         self._moving_right = False
 
-    def get_copy(self):
-        return self.create_new_obj(self.image, self._weapon, self.move_speed, self._health)
+    def copy(self):
+        pass
 
     def update(self):
         if self._moving_left:
@@ -24,32 +26,16 @@ class Ship(GameObject):
         if self._moving_right:
             self.x += self.move_speed
 
-    @is_values_type(int, bool)
+    @is_values_types(int, bool)
     def set_moving_flag(self, direction: int, value: bool):
         directions = {
             LEFT: '_moving_left',
             RIGHT: '_moving_right'
         }
-        if directions in directions.keys():
-            self.__setattr__(directions[direction], value)
-        else:
-            raise ValueError('Direction has to be LEFT=2 or RIGHT=3.'
-                             'Value has to be bool type.')
+        self.__setattr__(directions[direction], value)
 
     def attack(self):
-        self._weapon.attack(self.centerx, self.top)
-
-    @property
-    def health(self) -> int:
-        return self._health
-
-    @health.setter
-    @is_value_type(int)
-    def health(self, value: int):
-        if value >= 0:
-            self._health = value
-        else:
-            raise ValueError('Health has be from 0 and bigger.')
+        self._weapon.attack(self.centerx, self.top, self._group_projectiles)
 
     @property
     def weapon(self):
--- tests/test_ship.py
+++ tests/test_ship.py
@@ -12,34 +12,17 @@ class TestShip(unittest.TestCase):
     def setUp(self) -> None:
         image = Surface((32, 32))
         self.projectile = Projectile(image)
-        self.projectiles = Group()
-        self.weapon = Weapon(self.projectile, self.projectiles)
-        self.new_weapon = Weapon(self.projectile, self.projectiles)
-        self.ship = Ship(image, self.weapon)
-        self.correct_health = 10
-        self.incorrect_weapon = [1, 1.2, '1', 'das', object]
-        self.incorrect_health = [1.2, '1', 'ad', object]
-        self.incorrect_flag = ['1', 'adsf', object, 0, 1, 5, 7, 1.2, 2.3]
+        self.weapon = Weapon(self.projectile)
+        self.projectile_group = Group()
+        self.ship = Ship(image, self.weapon, self.projectile_group)
 
     def test_properties(self):
         #  Getters
         self.assertIsInstance(self.ship.weapon, Weapon)
-        self.assertIs(self.weapon, self.ship.weapon)
-        self.assertIsInstance(self.ship.health, int)
 
         # Setters
-        # Correct data
-        self.ship.weapon = self.new_weapon
+        self.ship.weapon = Weapon(self.projectile)
         self.assertIsNot(self.weapon, self.ship.weapon)
-        self.ship.health = self.correct_health
-        self.assertEqual(self.correct_health, self.ship.health)
-
-        # Incorrect data
-        with self.assertRaises((TypeError, ValueError)):
-            for data in self.incorrect_weapon:
-                self.ship.weapon = data
-            for data in self.incorrect_health:
-                self.ship.health = data
 
     def test_set_moving_flag(self):
         self.assertFalse(self.ship._moving_left)
@@ -49,37 +32,35 @@ class TestShip(unittest.TestCase):
         self.ship.set_moving_flag(LEFT, True)
         self.assertTrue(self.ship._moving_left)
 
-        with self.assertRaises(ValueError):
-            for data in self.incorrect_flag:
-                print(data)
-                self.ship.set_moving_flag(data, True)
-                self.ship.set_moving_flag(data, False)
-
     def test_update(self):
         self.ship.x = 0
-        self.ship.move_speed = 5
+        self.ship.move_speed = 10
         self.ship.set_moving_flag(RIGHT, True)
         self.ship.set_moving_flag(LEFT, False)
-        for i in range(10):
-            self.ship.update()
-            self.assertEqual(self.ship.move_speed * (i + 1), self.ship.x)
+        self.ship.update()
+        self.assertEqual(self.ship.move_speed, self.ship.x)
+        self.ship.update()
+        self.assertEqual(self.ship.move_speed * 2, self.ship.x)
 
+        self.ship.x = 0
         self.ship.set_moving_flag(RIGHT, False)
         self.ship.set_moving_flag(LEFT, True)
-        for i in range(10):
-            self.ship.update()
-            self.assertEqual(50 - self.ship.move_speed * (i + 1), self.ship.x)
+        self.ship.update()
+        self.assertEqual(-self.ship.move_speed, self.ship.x)
+        self.ship.update()
+        self.assertEqual(-self.ship.move_speed * 2, self.ship.x)
 
-        for i in range(10):
-            self.ship.set_moving_flag(RIGHT, True)
-            self.ship.set_moving_flag(LEFT, True)
-            self.assertEqual(0, self.ship.x)
+        self.ship.x = 0
+        self.ship.set_moving_flag(RIGHT, True)
+        self.ship.set_moving_flag(LEFT, True)
+        self.ship.update()
+        self.assertEqual(0, self.ship.x)
 
     def test_attack(self):
+        self.assertEqual(0, len(self.projectile_group.sprites()))
         self.ship.attack()
-        projectile = self.projectiles.sprites()[0]
-        self.assertIsInstance(projectile, Projectile)
-        self.assertIsNot(projectile, self.projectile)
+        self.assertEqual(1, len(self.projectile_group.sprites()))
+        projectile = self.projectile_group.sprites()[0]
         self.assertEqual(self.ship.centerx, projectile.centerx)
         self.assertEqual(self.ship.top, projectile.centery)
 
