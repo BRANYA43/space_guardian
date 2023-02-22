3926b9bc2631e3dfc5a25b10b8578469b0989e85
Timchenko Bogdan <tbd2504@.gmail.com>
2023-02-22 13:52:24 +0200

    Refactor Projectile and test

--- game/objects/game_objects/projectile.py
+++ game/objects/game_objects/projectile.py
@@ -3,20 +3,23 @@ import math
 from pygame import Surface
 from pygame.sprite import Sprite
 
-from ..base_object.game_object import GameObject
+from ..base_objects import GameObject
 from game.utils.decorators import is_value_type
-from game.utils.functions import get_valid_value
+from game.utils.functions import get_value_with_valid_type
 
 
 class Projectile(GameObject, Sprite):
     def __init__(self, image: Surface, *, move_speed: int = 5, damage: int = 1, angle: int = 270):
         Sprite.__init__(self)
         GameObject.__init__(self, image, move_speed=move_speed)
-        self._damage = get_valid_value(damage, int)
-        self._angle = get_valid_value(angle, int)
+        self._damage = get_value_with_valid_type(damage, int)
+        self._angle = get_value_with_valid_type(angle, int)
 
-    def get_copy(self):
-        return self.create_new_obj(self.image, move_speed=self.move_speed, damage=self._damage, agnle=self._angle)
+    def copy(self):
+        return self.create(self.image.copy(),
+                           move_speed=self.move_speed,
+                           damage=self._damage,
+                           angle=self._angle)
 
     def update(self):
         radian = self._angle * math.pi / 180
@@ -33,7 +36,7 @@ class Projectile(GameObject, Sprite):
         if value >= 0:
             self._damage = value
         else:
-            raise ValueError('Damage has to be from 0 and bigger. Damage >= 0.')
+            raise ValueError('Damage has to be from 0 and bigger.')
 
     @property
     def angle(self) -> int:
@@ -45,4 +48,4 @@ class Projectile(GameObject, Sprite):
         if 0 <= value < 360:
             self._angle = value
         else:
-            raise ValueError('Angle has to be from/equal 0 and smaller 360. 0 >= Angle < 360')
+            raise ValueError('Angle has to be from 0 to 360.')
--- tests/test_projectile.py
+++ tests/test_projectile.py
@@ -7,38 +7,46 @@ from game.objects import Projectile
 
 class TestProjectiles(unittest.TestCase):
     def setUp(self) -> None:
-        image = Surface(size=(32, 32))
-        self.projectile = Projectile(image)
-        self.correct_angle = 10
-        self.correct_damage = 10
-        self.incorrect_angles = (-1, -1.3, 1.3, '1', 'a', object, [], {}, 360, 1000)
-        self.incorrect_damages = (-1, -1.3, 1.3, '1', 'a', object, [], {})
+        image = Surface((32, 32))
+        self.obj = Projectile(image)
 
     def test_properties(self):
         # Getters
-        self.assertIsInstance(self.projectile.damage, int)
-        self.assertIsInstance(self.projectile.angle, int)
+        self.assertIsInstance(self.obj.damage, int)
+        self.assertIsInstance(self.obj.angle, int)
 
         # Setters
-        # Correct data
-        self.projectile.damage = self.correct_damage
-        self.projectile.angle = self.correct_angle
+        set_value = 100
+        self.obj.damage = set_value
+        self.assertEqual(set_value, self.obj.damage)
+        self.obj.angle = set_value
+        self.assertEqual(set_value, self.obj.angle)
 
-        self.assertEqual(self.correct_damage, self.projectile.damage)
-        self.assertEqual(self.correct_angle, self.projectile.angle)
+        with self.assertRaises(ValueError):
+            self.obj.damage = -1
+            self.obj.angle = -1
+            self.obj.angle = 360
+            self.obj.angle = 1000
 
-        # Incorrect data
-        with self.assertRaises((TypeError, ValueError)):
-            for value in self.incorrect_angles:
-                self.projectile.angle = value
-            for damage in self.incorrect_damages:
-                self.projectile.damage = damage
+    def test_update(self):
+        self.obj.angle = 90
+        self.obj.move_speed = 10
+        self.obj.update()
+        self.assertEqual(0, self.obj.x)
+        self.assertEqual(self.obj.move_speed, self.obj.y)
+        self.obj.update()
+        self.assertEqual(0, self.obj.x)
+        self.assertEqual(self.obj.move_speed * 2, self.obj.y)
 
-    def test_get_copy(self):
-        copy = self.projectile.get_copy()
-        self.assertIsNot(self.projectile, copy)
-        self.assertEqual(self.projectile.damage, copy.damage)
-        self.assertEqual(self.projectile.angle, copy.angle)
+    def test_copy(self):
+        copy = self.obj.copy()
+        self.assertIsInstance(copy, self.obj.__class__)
+        self.assertIsNot(self.obj, copy)
+        self.assertIsNot(self.obj.surface, copy.surface)
+        self.assertEqual(self.obj.move_speed, copy.move_speed)
+        self.assertEqual(self.obj.health, copy.health)
+        self.assertEqual(self.obj.damage, copy.damage)
+        self.assertEqual(self.obj.angle, copy.angle)
 
 
 if __name__ == '__main__':
